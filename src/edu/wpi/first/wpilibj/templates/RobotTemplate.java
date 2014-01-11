98b712181dcbbec587809f9b5799ee3cb5daeeba
Hall Chen <robotics.hchen@gmail.com>
2014-01-11 13:27:16 -0800

    first commit

---
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -0,0 +1,21 @@
+
+package edu.wpi.first.wpilibj.templates;
+
+
+import edu.wpi.first.wpilibj.SimpleRobot;
+
+
+public class RobotTemplate extends SimpleRobot {
+
+    public void autonomous() {
+        
+    }
+
+    public void operatorControl() {
+
+    }
+    
+    public void test() {
+    
+    }
+}
